ee4a37a23db775b6d8eb7c854bdbcf51f936e0f1
tomse <tomset111@gmail.com>
2023-05-06 14:30:12 +0200

    + Convert to json using custom case class + ParseJson before, then apply the conversion and convert to json format so the HttpEntity can properly make use of the application/json response format

--- src/main/scala/server.scala
+++ src/main/scala/server.scala
@@ -10,58 +10,73 @@ import scala.concurrent.{Await, ExecutionContextExecutor}
 import scala.io.StdIn
 import scala.util.{Failure, Success}
 import spray.json._
-import DefaultJsonProtocol._
 
 object server extends App {
-    implicit val actorSystem: ActorSystem = ActorSystem()
-    implicit val ec: ExecutionContextExecutor = actorSystem.dispatcher
-
-    implicit class MapToJson[V](params: Map[String, V]) {
-      def toUrlParams: String = params.map { case (k, v) => s"$k=$v" }.mkString("&")
-    }
-
-    private val route = {
-      concat(
-        path("api") {
-          get {
-            parameters("platform", Symbol("category")) { (platform, category)  =>
-              val requestParams = Map(
-                "platform" -> platform,
-                "category" -> category
-              ).toUrlParams
-
-              val freeTopGameRequest = HttpRequest(
-                method = HttpMethods.GET,
-                uri = s"https://www.freetogame.com/api/games?$requestParams",
-                headers = Seq(
-                  RawHeader("Accept", "application/json")
-                )
-              )
-
-              val responseFuture = Http().singleRequest(freeTopGameRequest)
-              val awaitResponse = Await.result(responseFuture.flatMap(resp => Unmarshal(resp.entity).to[String]), 10.seconds)
-              val jsonResponse = awaitResponse.parseJson
-
-              responseFuture.value match {
-                case Some(Success(_)) => complete(HttpEntity(ContentTypes.`application/json`, s"$jsonResponse"))
-                case Some(Failure(e)) => complete(InternalServerError, s"An error occurred while processing your request: $e")
-              }
-            }
-          }
-        },
-        pathEndOrSingleSlash {
-          get {
-            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "api path not given!"))
-          }
-        }
-      )
-    }
-
-    private val bindingFut = for {
-      binding <- Http().newServerAt("localhost", 8080).bind(route)
-      _ = println(s"Server running on ${binding.localAddress.getHostName}:${binding.localAddress.getPort}")
-    } yield binding
-    StdIn.readLine()
-    bindingFut.flatMap(_.unbind()).andThen(_ => actorSystem.terminate())
+  private case class Game(
+                   title: String,
+                   thumbnail: String,
+                   short_description: String,
+                   game_url: String,
+                   genre: String,
+                   platform: String,
+                   publisher: String,
+                   developer: String,
+                   release_date: String,
+                   freetogame_profile_url: String
+                 )
+   private object GameProtocol extends DefaultJsonProtocol {
+    implicit val gameFormat: RootJsonFormat[Game] = jsonFormat10(Game)
   }
 
+  implicit val actorSystem: ActorSystem = ActorSystem()
+  implicit val ec: ExecutionContextExecutor = actorSystem.dispatcher
+
+  implicit class MapToJson[V](params: Map[String, V]) {
+    def toUrlParams: String = params.map { case (k, v) => s"$k=$v" }.mkString("&")
+  }
+
+  private val route = {
+    import GameProtocol._
+    concat(
+      path("api") {
+        get {
+          parameters("platform", Symbol("category")) { (platform, category)  =>
+            val requestParams = Map(
+              "platform" -> platform,
+              "category" -> category
+            ).toUrlParams
+
+            val freeTopGameRequest = HttpRequest(
+              method = HttpMethods.GET,
+              uri = s"https://www.freetogame.com/api/games?$requestParams",
+              headers = Seq(
+                RawHeader("Accept", "application/json")
+              )
+            )
+
+            val responseFuture = Http().singleRequest(freeTopGameRequest)
+            val awaitResponse = Await.result(responseFuture.flatMap(resp => Unmarshal(resp.entity).to[String]), 10.seconds)
+            val jsonResponse = awaitResponse.parseJson.convertTo[List[Game]].toJson
+
+            responseFuture.value match {
+              case Some(Success(_)) => complete(HttpEntity(ContentTypes.`application/json`, s"$jsonResponse"))
+              case Some(Failure(e)) => complete(InternalServerError, s"An error occurred while processing your request: $e")
+            }
+          }
+        }
+      },
+      pathEndOrSingleSlash {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "api path not given!"))
+        }
+      }
+    )
+  }
+
+  private val bindingFut = for {
+    binding <- Http().newServerAt("localhost", 8080).bind(route)
+    _ = println(s"Server running on ${binding.localAddress.getHostName}:${binding.localAddress.getPort}")
+  } yield binding
+  StdIn.readLine()
+  bindingFut.flatMap(_.unbind()).andThen(_ => actorSystem.terminate())
+}
